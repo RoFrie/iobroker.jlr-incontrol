d79f27ad9e695743c42accbd2dc7b5ddf29f5fcc
root <root@frvo.de>
2020-12-22 12:00:59 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"jlr-incontrol adapter settings": {
+		"en": "Adapter settings for jlr-incontrol",
+		"de": "Adaptereinstellungen für jlr-incontrol",
+		"ru": "Настройки адаптера для jlr-incontrol",
+		"pt": "Configurações do adaptador para jlr-incontrol",
+		"nl": "Adapterinstellingen voor jlr-incontrol",
+		"fr": "Paramètres d'adaptateur pour jlr-incontrol",
+		"it": "Impostazioni dell'adattatore per jlr-incontrol",
+		"es": "Ajustes del adaptador para jlr-incontrol",
+		"pl": "Ustawienia adaptera dla jlr-incontrol",
+		"zh-cn": "jlr-incontrol的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
